2531f989c8d3b5c32b2c7b604bb9c643457eee61
Humberto Bravo <humbandroid@gmail.com>
2022-05-04 19:50:15 -0300

    VisualBasicTestProject {HN Labs} - Testando a Linguagem Visual Basic.

---
+++ public/tester.vb
@@ -0,0 +1,42 @@
+Dim frd As Integer = 1;
+
+Dim fde As Char = "a";
+
+Dim rre As Boolean = True;
+
+Dim fes As String = "Hello World";
+
+Dim rre As Byte = 1;
+
+Function f() As Integer
+    Return 1;
+End Function
+
+Class c
+    Public Sub New()
+    End Sub
+End Class
+
+For index As ObjectType = lower To Upper
+  
+Next index
+
+While Boolean
+  
+End While
+
+Do
+  
+Loop While Boolean
+
+End While
+
+Imports System.Collections.Generic
+
+Module Module1
+    Sub Main()
+        Dim a As Integer = 1;
+        Dim b As Integer = 2;
+        Dim c As Integer = 3;
+    End Sub
+End Module
